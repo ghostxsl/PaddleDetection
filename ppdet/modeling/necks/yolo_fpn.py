0a3f648fe4e3c60e09928d0316d41a9f37385492
ghostxsl <451323469@qq.com>
2021-09-01 15:43:12 +0800

    add BiFPN

--- ppdet/modeling/necks/yolo_fpn.py
+++ ppdet/modeling/necks/yolo_fpn.py
@@ -379,6 +379,73 @@ class PPYOLODetBlockCSP(nn.Layer):
         return conv, conv
 
 
+class PPYOLODetBlockCSPv2(nn.Layer):
+    def __init__(self,
+                 cfg,
+                 ch_in,
+                 ch_out,
+                 act,
+                 norm_type,
+                 name,
+                 data_format='NCHW'):
+        """
+        PPYOLODetBlockCSP layer
+
+        Args:
+            cfg (list): layer configs for this block
+            ch_in (int): input channel
+            ch_out (int): output channel
+            act (str): default mish
+            name (str): block name
+            data_format (str): data format, NCHW or NHWC
+        """
+        super(PPYOLODetBlockCSPv2, self).__init__()
+        self.data_format = data_format
+        self.conv1 = ConvBNLayer(
+            ch_in,
+            ch_out,
+            1,
+            padding=0,
+            act=act,
+            norm_type=norm_type,
+            name=name + '.left',
+            data_format=data_format)
+        self.conv2 = ConvBNLayer(
+            ch_in,
+            ch_out,
+            1,
+            padding=0,
+            act=act,
+            norm_type=norm_type,
+            name=name + '.right',
+            data_format=data_format)
+        self.conv3 = ConvBNLayer(
+            ch_out * 2,
+            ch_out,
+            1,
+            padding=0,
+            act=act,
+            norm_type=norm_type,
+            name=name,
+            data_format=data_format)
+        self.conv_module = nn.Sequential()
+        for idx, (layer_name, layer, args, kwargs) in enumerate(cfg):
+            kwargs.update(name=name + layer_name, data_format=data_format)
+            self.conv_module.add_sublayer(layer_name, layer(*args, **kwargs))
+
+    def forward(self, inputs):
+        conv_left = self.conv1(inputs)
+        conv_right = self.conv2(inputs)
+        conv_left = self.conv_module(conv_left)
+        if self.data_format == 'NCHW':
+            conv = paddle.concat([conv_left, conv_right], axis=1)
+        else:
+            conv = paddle.concat([conv_left, conv_right], axis=-1)
+
+        conv = self.conv3(conv)
+        return conv, conv
+
+
 @register
 @serializable
 class YOLOv3FPN(nn.Layer):
@@ -986,3 +1053,203 @@ class PPYOLOPAN(nn.Layer):
     @property
     def out_shape(self):
         return [ShapeSpec(channels=c) for c in self._out_channels]
+
+
+@register
+@serializable
+class PPYOLOPANv2(nn.Layer):
+    __shared__ = ['norm_type', 'data_format']
+
+    def __init__(self,
+                 in_channels=(512, 1024, 2048),
+                 out_channel=256,
+                 num_extra_levels=2,
+                 norm_type='bn',
+                 data_format='NCHW',
+                 act='mish',
+                 conv_block_num=3,
+                 drop_block=False,
+                 block_size=3,
+                 keep_prob=0.9,
+                 spp=False):
+        """
+        PPYOLOPAN layer with SPP, DropBlock and CSP connection.
+
+        Args:
+            in_channels (list): input channels for fpn
+            norm_type (str): batch norm type, default bn
+            data_format (str): data format, NCHW or NHWC
+            act (str): activation function, default mish
+            conv_block_num (int): conv block num of each pan block
+            drop_block (bool): whether use DropBlock or not
+            block_size (int): block size of DropBlock
+            keep_prob (float): keep probability of DropBlock
+            spp (bool): whether use spp or not
+
+        """
+        super(PPYOLOPANv2, self).__init__()
+        assert len(in_channels) > 0, "in_channels length should > 0"
+        assert num_extra_levels >= 0, "num_extra_levels length should >= 0"
+
+        self.in_channels = in_channels
+        self.out_channel = out_channel
+        self.num_extra_levels = num_extra_levels
+        self.num_levels = len(in_channels) + self.num_extra_levels
+
+        # parse kwargs
+        self.drop_block = drop_block
+        self.block_size = block_size
+        self.keep_prob = keep_prob
+        self.spp = spp
+        self.conv_block_num = conv_block_num
+        self.data_format = data_format
+        dropblock_cfg = []
+        if self.drop_block:
+            dropblock_cfg = [[
+                'dropblock', DropBlock, [self.block_size, self.keep_prob],
+                dict()
+            ]]
+
+        # lateral conv
+        self.lateral_convs = nn.LayerList()
+        for in_c in self.in_channels:
+            self.lateral_convs.append(
+                ConvBNLayer(
+                    in_c, self.out_channel, 1, act='', data_format=data_format))
+        if self.num_extra_levels > 0:
+            self.extra_convs = nn.LayerList()
+            for i in range(self.num_extra_levels):
+                if i == 0:
+                    self.extra_convs.append(
+                        ConvBNLayer(
+                            self.in_channels[-1],
+                            self.out_channel,
+                            3,
+                            2,
+                            padding=1,
+                            act='',
+                            data_format=data_format))
+                else:
+                    self.extra_convs.append(
+                        nn.MaxPool2D(
+                            3, 2, 1, data_format=data_format))
+
+        # fpn: P7 -> P3
+        self.fpn_blocks = []
+        for i in range(self.num_levels):
+            base_cfg = []
+            for j in range(self.conv_block_num):
+                base_cfg += [
+                    # name, layer, args
+                    [
+                        '{}.0'.format(j), ConvBNLayer,
+                        [self.out_channel, self.out_channel, 1], dict(
+                            padding=0, act=act, norm_type=norm_type)
+                    ],
+                    [
+                        '{}.1'.format(j), ConvBNLayer,
+                        [self.out_channel, self.out_channel, 3], dict(
+                            padding=1, act=act, norm_type=norm_type)
+                    ]
+                ]
+
+            if i == 0 and self.spp:
+                base_cfg[3] = [
+                    'spp', SPP, [self.out_channel * 4, self.out_channel, 1],
+                    dict(
+                        pool_size=[5, 9, 13], act=act, norm_type=norm_type)
+                ]
+
+            cfg = base_cfg[:4] + dropblock_cfg + base_cfg[4:]
+            name = 'fpn.{}'.format(i)
+            fpn_block = self.add_sublayer(
+                name,
+                PPYOLODetBlockCSPv2(cfg, self.out_channel, self.out_channel,
+                                    act, norm_type, name, data_format))
+            self.fpn_blocks.append(fpn_block)
+
+        # pan: P3 -> P7
+        self.pan_blocks = []
+        self.pan_down = nn.LayerList()
+        for i in range(self.num_levels - 1):
+            base_cfg = []
+            for j in range(self.conv_block_num):
+                base_cfg += [
+                    # name, layer, args
+                    [
+                        '{}.0'.format(j), ConvBNLayer,
+                        [self.out_channel, self.out_channel, 1], dict(
+                            padding=0, act=act, norm_type=norm_type)
+                    ],
+                    [
+                        '{}.1'.format(j), ConvBNLayer,
+                        [self.out_channel, self.out_channel, 3], dict(
+                            padding=1, act=act, norm_type=norm_type)
+                    ]
+                ]
+
+            cfg = base_cfg[:4] + dropblock_cfg + base_cfg[4:]
+            name = 'pan.{}'.format(i)
+            pan_block = self.add_sublayer(
+                name,
+                PPYOLODetBlockCSPv2(cfg, self.out_channel, self.out_channel,
+                                    act, norm_type, name, data_format))
+            self.pan_blocks.append(pan_block)
+            self.pan_down.append(
+                ConvBNLayer(
+                    ch_in=self.out_channel,
+                    ch_out=self.out_channel,
+                    filter_size=3,
+                    stride=2,
+                    padding=1,
+                    act=act,
+                    norm_type=norm_type,
+                    data_format=data_format,
+                    name=name))
+
+    def forward(self, blocks, for_mot=False):
+        assert len(blocks) == len(self.in_channels)
+
+        feats = []
+        for conv_layer, feature in zip(self.lateral_convs, blocks):
+            feats.append(conv_layer(feature))
+        if self.num_extra_levels > 0:
+            feat = blocks[-1]
+            for conv_layer in self.extra_convs:
+                feat = conv_layer(feat)
+                feats.append(feat)
+        # fpn
+        fpn_feats = []
+        for i, (feature,
+                fpn_block) in enumerate(zip(feats[::-1], self.fpn_blocks)):
+            if i == 0:
+                route, tip = fpn_block(feature)
+            else:
+                shape = paddle.shape(feature)
+                route = F.interpolate(
+                    route,
+                    size=[shape[2], shape[3]],
+                    data_format=self.data_format)
+                route, tip = fpn_block(feature + route)
+            fpn_feats.append(tip)
+        # pan
+        pan_feats = []
+        for i, feature in enumerate(fpn_feats[::-1]):
+            if i == 0:
+                pan_feats.append(feature)
+                route = feature
+            else:
+                route = self.pan_down[i - 1](route)
+                route, tip = self.pan_blocks[i - 1](route + feature)
+                pan_feats.append(tip)
+        return pan_feats
+
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        return {'in_channels': [i.channels for i in input_shape], }
+
+    @property
+    def out_shape(self):
+        return [
+            ShapeSpec(channels=self.out_channel) for _ in range(self.num_levels)
+        ]
